a5d74af59bd4e0dc745b6ac22a948fa99b5ea7e0
ValberJunior <valberjr1992@gmail.com>
2021-11-08 18:08:52 -0300

    update

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+const Home = ()=>{
+    return(
+        <Container>
+            <h1>Hello World :)</h1>
+        </Container>
+    )
+}
+
+export default Home;
---
+++ src/themes.js
@@ -0,0 +1,15 @@
+
+const theme = {
+    color:{
+        primary: '#6200ee',
+        background: '#00000014',
+        text: '#000000B3',
+        success: '#135E01'
+
+    },
+    font:{
+        regular: 'Roboto, sans-serif',
+    },
+};
+
+export default theme;
