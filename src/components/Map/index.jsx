e00f8896cc65e94b9ffc43850f2540843a159d28
ValberJunior <valberjr1992@gmail.com>
2021-11-10 00:40:43 -0300

    search and location

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,10 +1,36 @@
 import { GoogleApiWrapper, Map, Marker} from "google-maps-react";
+import { useState } from "react";
 
 export const MapContainer = (props)=>{
 
+    const [map, setMap] = useState(null);
+
     const { google } = props;
 
-    return(<Map google={google} centerAroundCurrentLocation />)
+    function searchNearby(map, center){
+        const service = new google.maps.places.PlacesService(map);
+
+        const request = {
+            location: center,
+            radius: '20000',
+            type: ['restaurant'],
+        };
+
+        service.nearbySearch(request, (results, status)=>{
+            if(status === google.maps.places.PlacesService.ok)
+            {
+                console.log('restaurants ',results)
+            }
+        })
+    }
+
+    function onMapReady(_, map){
+        setMap(map);
+        searchNearby(map, map.center);
+
+    }
+
+    return(<Map google={google} centerAroundCurrentLocation onReady={onMapReady} onRecenter={onMapReady} />)
 
 };
 
